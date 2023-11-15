9de575140b12f55249c4fe4951da53565547f57c
Thowru <87294208+Thowru@users.noreply.github.com>
2023-11-15 14:57:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,56 @@
 import streamlit as st
 import pandas as pd
-from datetime import datetime
 import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
 
+# K-means 모델 학습 및 예측 함수
+def kmeans_clustering(df, cols_to_train, n_clusters=2):
+    model = KMeans(n_clusters=n_clusters, random_state=42)
+    model.fit(df[cols_to_train])
 
+    # 클러스터 할당 및 결과 컬럼 추가
+    df['cluster_kmeans'] = model.predict(df[cols_to_train])
 
+    return df, model
+
+# 시각화 함수
+def visualize_kmeans_clusters(df, model, cols_to_visualize):
+    plt.scatter(df[cols_to_visualize[0]], df[cols_to_visualize[1]], c=df['cluster_kmeans'], s=60)
+    plt.scatter(model.cluster_centers_[:, 0], model.cluster_centers_[:, 1], c='red', marker='^', s=100)
+    plt.xlabel(cols_to_visualize[0])
+    plt.ylabel(cols_to_visualize[1])
+    plt.title("K-means 클러스터링 결과")
+
+    # Streamlit에 Matplotlib 그래프 표시
+    st.pyplot()
+
+# Streamlit 앱 함수
 def main():
-    # st.title('로그 데이터 처리 앱')
+    st.title('K-means 클러스터링 결과 시각화')
 
     # 파일 업로드
-    # uploaded_csvfile = st.file_uploader("CSV 파일 선택", type="csv")
+    uploaded_csvfile = st.file_uploader("CSV 파일 선택", type="csv")
 
-    # if uploaded_csvfile is not None:
-    # CSV 파일 읽기
-    df_entity = pd.read_csv('train_processed.csv')
-    
+    if uploaded_csvfile is not None:
+        # CSV 파일 읽기
+        df_entity = pd.read_csv(uploaded_csvfile)
 
-    # 선택할 feature들
-    cols_to_train = ['method_cnt', 'method_post', 'protocol_1_0', 'status_major', 'status_404', 'status_499', 'status_cnt',
-                     'path_same', 'path_xmlrpc', 'ua_cnt', 'has_payload', 'bytes_avg', 'bytes_std']
-    
-    # PCA를 사용하여 데이터의 차원을 2로 축소
-    pca = PCA(n_components=2)
-    pca_result = pca.fit_transform(df_entity[cols_to_train])
-    
-    # PCA 결과를 데이터프레임에 추가
-    df_entity['pca_1'] = pca_result[:, 0]
-    df_entity['pca_2'] = pca_result[:, 1]
-    
-    # Streamlit 앱 레이아웃
-    st.title("전체 Feature를 이용한 이상탐지된 Entity 시각화 (PCA 결과)")
-    
-    # 그래프 그리기
-    fig, ax = plt.subplots(figsize=(10, 6))
-    scatter = ax.scatter(df_entity['pca_1'], df_entity['pca_2'], c=df_entity['cluster_kmeans'], cmap='viridis', s=60)
-    ax.set_xlabel("PCA 1")
-    ax.set_ylabel("PCA 2")
-    ax.set_title("전체 Feature를 이용한 이상탐지된 Entity 시각화 (PCA 결과)")
-    ax.legend(*scatter.legend_elements(), title='클러스터')
-    
-    # Streamlit에 Matplotlib 그래프 표시
-    st.pyplot(fig)
-   
-        
+        # 클러스터링을 위한 feature 선택
+        cols_to_train = ['method_post', 'status_404']
+
+        # K-means 클러스터링 수행
+        df_entity, kmeans_model = kmeans_clustering(df_entity, cols_to_train)
+
+        # K-means 클러스터 시각화
+        visualize_kmeans_clusters(df_entity, kmeans_model, cols_to_train)
+
+        # 클러스터별 데이터 포인트 수 출력
+        st.write("각 클러스터의 데이터 포인트 수:")
+        st.write(df_entity['cluster_kmeans'].value_counts())
+
+        # 클러스터 0에 속한 Host 출력
+        st.write("클러스터 0에 속한 Host:")
+        st.write(df_entity[df_entity['cluster_kmeans'] == 0]['Host'])
 
 if __name__ == '__main__':
     main()
