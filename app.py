4133e7068bdbc1f2c4fab5bcb0cf27b24c9d8746
Thowru <87294208+Thowru@users.noreply.github.com>
2023-11-15 14:59:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 
 # K-means 모델 학습 및 예측 함수
 def kmeans_clustering(df, cols_to_train, n_clusters=2):
@@ -13,20 +17,35 @@ def kmeans_clustering(df, cols_to_train, n_clusters=2):
 
     return df, model
 
-# 시각화 함수
-def visualize_kmeans_clusters(df, model, cols_to_visualize):
-    plt.scatter(df[cols_to_visualize[0]], df[cols_to_visualize[1]], c=df['cluster_kmeans'], s=60)
-    plt.scatter(model.cluster_centers_[:, 0], model.cluster_centers_[:, 1], c='red', marker='^', s=100)
-    plt.xlabel(cols_to_visualize[0])
-    plt.ylabel(cols_to_visualize[1])
-    plt.title("K-means 클러스터링 결과")
+# PCA를 사용하여 데이터의 차원을 2로 축소
+def apply_pca(df, cols_to_train):
+    pca = PCA(n_components=2)
+    pca_result = pca.fit_transform(df[cols_to_train])
+    df['pca_1'] = pca_result[:, 0]
+    df['pca_2'] = pca_result[:, 1]
+    return df
 
-    # Streamlit에 Matplotlib 그래프 표시
+# t-SNE를 사용하여 데이터의 차원을 2로 축소
+def apply_tsne(df, cols_to_train):
+    tsne = TSNE(n_components=2, verbose=0, perplexity=40, n_iter=500, random_state=42)
+    tsne_results = tsne.fit_transform(df[cols_to_train])
+    df['tsne_2d_one'] = tsne_results[:, 0]
+    df['tsne_2d_two'] = tsne_results[:, 1]
+    return df
+
+# 시각화 함수
+def visualize_clusters_2d(df, x_col, y_col, hue_col, title):
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(x=x_col, y=y_col, data=df, hue=hue_col, palette='viridis', s=60)
+    plt.xlabel(x_col)
+    plt.ylabel(y_col)
+    plt.title(title)
+    plt.colorbar(label='클러스터')
     st.pyplot()
 
 # Streamlit 앱 함수
 def main():
-    st.title('K-means 클러스터링 결과 시각화')
+    st.title('K-means 및 차원 축소 결과 시각화')
 
     # 파일 업로드
     uploaded_csvfile = st.file_uploader("CSV 파일 선택", type="csv")
@@ -36,21 +55,25 @@ def main():
         df_entity = pd.read_csv(uploaded_csvfile)
 
         # 클러스터링을 위한 feature 선택
-        cols_to_train = ['method_post', 'status_404']
+        cols_to_train_kmeans = ['method_post', 'status_404']
 
         # K-means 클러스터링 수행
-        df_entity, kmeans_model = kmeans_clustering(df_entity, cols_to_train)
+        df_entity, kmeans_model = kmeans_clustering(df_entity, cols_to_train_kmeans)
 
         # K-means 클러스터 시각화
-        visualize_kmeans_clusters(df_entity, kmeans_model, cols_to_train)
+        visualize_clusters_2d(df_entity, 'pca_1', 'pca_2', 'cluster_kmeans', 'K-means 클러스터링 결과 (PCA)')
 
-        # 클러스터별 데이터 포인트 수 출력
-        st.write("각 클러스터의 데이터 포인트 수:")
-        st.write(df_entity['cluster_kmeans'].value_counts())
+        # PCA를 사용한 차원 축소
+        df_entity = apply_pca(df_entity, cols_to_train_kmeans)
 
-        # 클러스터 0에 속한 Host 출력
-        st.write("클러스터 0에 속한 Host:")
-        st.write(df_entity[df_entity['cluster_kmeans'] == 0]['Host'])
+        # PCA 시각화
+        visualize_clusters_2d(df_entity, 'pca_1', 'pca_2', 'cluster_kmeans', 'PCA를 사용한 차원 축소 결과')
+
+        # t-SNE를 사용한 차원 축소
+        df_entity = apply_tsne(df_entity, cols_to_train_kmeans)
+
+        # t-SNE 시각화
+        visualize_clusters_2d(df_entity, 'tsne_2d_one', 'tsne_2d_two', 'cluster_kmeans', 't-SNE를 사용한 차원 축소 결과')
 
 if __name__ == '__main__':
     main()
