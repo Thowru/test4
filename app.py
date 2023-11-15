c7874b080258c741871a155ff5a04a3e98530b38
Thowru <87294208+Thowru@users.noreply.github.com>
2023-11-15 21:23:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,10 +16,11 @@ from process_log import process_log_data
 from feature_extraction import feature_extract
 from feature_extraction import feature_extract2
 
+
 rc('font', family='NanumGothic')
 
 def main():
-    st.title('colab 코드 띄우기')
+    st.title('Colab 코드 띄우기')
 
     # 파일 업로드
     uploaded_csvfile = st.file_uploader("CSV 파일 선택", type="csv")
@@ -28,20 +29,18 @@ def main():
         # 업로드된 파일 읽기
         df_entity = pd.read_csv(uploaded_csvfile, index_col='entity')
 
-        # 나머지 코드 실행
+        # Feature Scaling
         columns_to_scale = ['method_cnt', 'status_cnt', 'ua_cnt', 'bytes_avg', 'bytes_std']
         scaler = preprocessing.MinMaxScaler()
-        scaler = scaler.fit(df_entity[columns_to_scale])
-        df_entity[columns_to_scale] = scaler.transform(df_entity[columns_to_scale])
+        df_entity[columns_to_scale] = scaler.fit_transform(df_entity[columns_to_scale])
 
-        cols_to_train = ['method_cnt','method_post','protocol_1_0','status_major','status_404','status_499','status_cnt','path_same','path_xmlrpc','ua_cnt','has_payload','bytes_avg','bytes_std']
+        # Clustering
+        cols_to_train = ['method_cnt', 'method_post', 'protocol_1_0', 'status_major', 'status_404', 'status_499', 'status_cnt', 'path_same', 'path_xmlrpc', 'ua_cnt', 'has_payload', 'bytes_avg', 'bytes_std']
         kmeans = KMeans(n_clusters=2, random_state=42)
-        
-        # 정상/ 비정상 클러스터로 나누어 보기
         kmeans.fit(df_entity[cols_to_train])
 
+        # 결과 표시
         df_entity['cluster_kmeans'] = kmeans.predict(df_entity[cols_to_train])
-
         st.write(df_entity['cluster_kmeans'].value_counts())
         st.write(df_entity[df_entity['cluster_kmeans']==0].index)
 
