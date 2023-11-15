bc7236757d9f979d483ed986936088c3489837f0
Thowru <87294208+Thowru@users.noreply.github.com>
2023-11-15 21:28:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,16 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
-from datetime import datetime
-import seaborn as sns
-import matplotlib.pyplot as plt
-import platform
-from matplotlib import font_manager, rc
 from sklearn import preprocessing
-from sklearn.cluster import KMeans, DBSCAN
+from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.manifold import TSNE
-from process_log import process_log_data 
-from feature_extraction import feature_extract
-from feature_extraction import feature_extract2
+import matplotlib.pyplot as plt
 
-
-rc('font', family='NanumGothic')
+# 그래프 한글 폰트 설정
+plt.rc('font', family='NanumGothic')
 
 def main():
-    st.title('Colab 코드 띄우기')
+    st.title('Entity 클러스터링 및 PCA 시각화')
 
     # 파일 업로드
     uploaded_csvfile = st.file_uploader("CSV 파일 선택", type="csv")
@@ -41,8 +31,11 @@ def main():
 
         # 결과 표시
         df_entity['cluster_kmeans'] = kmeans.predict(df_entity[cols_to_train])
+        st.write("클러스터별 데이터 수:")
         st.write(df_entity['cluster_kmeans'].value_counts())
-        st.write(df_entity[df_entity['cluster_kmeans']==0].index)
+
+        st.write("클러스터 0에 속한 Entity:")
+        st.write(df_entity[df_entity['cluster_kmeans'] == 0].index)
 
         # PCA를 사용하여 데이터의 차원을 2로 축소
         pca = PCA(n_components=2)
@@ -57,10 +50,11 @@ def main():
         plt.scatter(df_entity['pca_1'], df_entity['pca_2'], c=df_entity['cluster_kmeans'], cmap='viridis', s=60)
         plt.xlabel("PCA 1")
         plt.ylabel("PCA 2")
-        plt.title("전체 Feature를 이용한 이상탐지된 Entity 시각화 (PCA 결과)")
+        plt.title("전체 Feature를 이용한 클러스터링된 Entity 시각화 (PCA 결과)")
         plt.colorbar(label='클러스터')
 
         st.pyplot(fig)
 
 if __name__ == '__main__':
     main()
+`
