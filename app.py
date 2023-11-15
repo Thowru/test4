16637882c43517ae4173b1e42547ccc9c91c7bf7
Thowru <87294208+Thowru@users.noreply.github.com>
2023-11-15 15:57:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,102 +1,87 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.cluster import KMeans, DBSCAN
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.manifold import TSNE
 
-# Function to perform K-means clustering
-def perform_kmeans(data, n_clusters=2):
-    kmeans_model = KMeans(n_clusters=n_clusters, random_state=42)
-    data['cluster_kmeans'] = kmeans_model.fit_predict(data)
-    return data, kmeans_model
+def feature_extract(df):
+    df['method_cnt'] = 0.0
+    df['method_post'] = 0.0
+    df['protocol_1_0'] = False
+    df['status_major'] = 0.0
+    df['status_404'] = 0.0
+    df['status_499'] = False
+    df['status_cnt'] = 0.0
+    df['path_same'] = 0.0
+    df['path_xmlrpc'] = True
+    df['ua_cnt'] = 0.0
+    df['has_payload'] = False
+    df['bytes_avg'] = 0.0
+    df['bytes_std'] = 0.0
 
-# Function to perform DBSCAN clustering
-def perform_dbscan(data, eps=0.5, min_samples=2):
-    dbscan_model = DBSCAN(eps=eps, min_samples=min_samples)
-    data['cluster_dbscan'] = dbscan_model.fit_predict(data)
-    return data, dbscan_model
+    cnt = 0
+    for entity in df['Host'].unique():
+        if cnt % 500 == 0:
+            print(cnt)
 
-# Function to visualize 2D clusters
-def visualize_clusters_2d(data, x_col, y_col, hue_col, title):
-    plt.figure(figsize=(10, 6))
-    scatter = sns.scatterplot(x=x_col, y=y_col, data=data, hue=hue_col, palette='viridis', s=60)
-    plt.xlabel(x_col)
-    plt.ylabel(y_col)
-    plt.title(title)
+        group = df[df['Host'] == entity]
 
-    # Extract the mappable from the scatter plot
-    handles, labels = scatter.get_legend_handles_labels()
-    mappable = handles[0]
+        method_cnt = group['Method'].nunique()
+        df.loc[df['Host'] == entity, 'method_cnt'] = method_cnt
 
-    # Add colorbar using the mappable
-    plt.colorbar(mappable, label='클러스터')
+        method_post_percent = len(group[group['Method'] == 'POST']) / max(1, len(group))
+        df.loc[df['Host'] == entity, 'method_post'] = method_post_percent
 
-    st.pyplot()
+        use_1_0 = any(group['Protocol'] == 'HTTP/1.0')
+        df.loc[df['Host'] == entity, 'protocol_1_0'] = use_1_0
 
-# Streamlit app
+        status_major_percent = len(group[group['Status'].isin(['200', '301', '302'])]) / max(1, len(group))
+        df.loc[df['Host'] == entity, 'status_major'] = status_major_percent
+
+        status_404_percent = len(group[group['Status'] == '404']) / max(1, len(group))
+        df.loc[df['Host'] == entity, 'status_404'] = status_404_percent
+
+        has_499 = any(group['Status'] == '499')
+        df.loc[df['Host'] == entity, 'status_499'] = has_499
+
+        status_cnt = group['Status'].nunique()
+        df.loc[df['Host'] == entity, 'status_cnt'] = status_cnt
+
+        top1_path_cnt = group['Path'].value_counts().iloc[0] if not group['Path'].value_counts().empty else 0
+        path_same = top1_path_cnt / max(1, len(group))
+        df.loc[df['Host'] == entity, 'path_same'] = path_same
+
+        path_xmlrpc = len(group[group['Path'].str.contains('xmlrpc.php') == True]) / max(1, len(group))
+        df.loc[df['Host'] == entity, 'path_xmlrpc'] = path_xmlrpc
+
+        ua_cnt = group['UA'].nunique()
+        df.loc[df['Host'] == entity, 'ua_cnt'] = ua_cnt
+
+        has_payload = any(group['Payload'] != '-')
+        df.loc[df['Host'] == entity, 'has_payload'] = has_payload
+
+        bytes_avg = np.mean(group['Bytes'])
+        bytes_std = np.std(group['Bytes'])
+        df.loc[df['Host'] == entity, 'bytes_avg'] = bytes_avg
+        df.loc[df['Host'] == entity, 'bytes_std'] = bytes_std
+
+        cnt += 1
+    return df
+
 def main():
-    st.title("Anomaly Detection with Streamlit")
+    st.title('로그 데이터 처리 앱')
 
-    # File Upload
-    st.sidebar.header("Upload CSV File")
-    uploaded_file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
+    # 파일 업로드
+    uploaded_csvfile = st.file_uploader("CSV 파일 선택", type="csv")
 
-    # K-means parameters
-    n_clusters = st.sidebar.slider("Number of K-means clusters", 2, 10, 2)
+    if uploaded_csvfile is not None:
+        # CSV 파일 읽기
+        df_entity = pd.read_csv(uploaded_csvfile)
 
-    # DBSCAN parameters
-    eps = st.sidebar.slider("DBSCAN Epsilon", 0.1, 2.0, 0.5)
-    min_samples = st.sidebar.slider("DBSCAN Minimum Samples", 1, 10, 2)
+        # Feature Extraction
+        df_entity_processed = feature_extract(df_entity)
 
-    if uploaded_file is not None:
-        # Read CSV file
-        df = pd.read_csv(uploaded_file)
+        # 전처리된 데이터 출력
+        st.write("전처리된 데이터:")
+        st.write(df_entity_processed)
 
-        # Display the DataFrame
-        st.subheader("Data Preview")
-        st.dataframe(df)
-
-        # K-means clustering
-        st.subheader("K-means Clustering")
-        df, kmeans_model = perform_kmeans(df, n_clusters)
-        st.write("K-means clustering results:")
-        st.write("Cluster Centers:", kmeans_model.cluster_centers_)
-        st.write("Cluster Labels:", df['cluster_kmeans'].value_counts())
-
-        # DBSCAN clustering
-        st.subheader("DBSCAN Clustering")
-        df, dbscan_model = perform_dbscan(df, eps, min_samples)
-        st.write("DBSCAN clustering results:")
-        st.write("Core sample indices:", dbscan_model.core_sample_indices_)
-        st.write("Cluster Labels:", df['cluster_dbscan'].value_counts())
-
-        # Visualize 2D clusters
-        st.subheader("Visualize Clusters (2D)")
-        visualize_clusters_2d(df, 'method_post', 'status_404', 'cluster_kmeans', 'K-means Clustering Results')
-
-        # Visualize anomalies
-        st.subheader("Visualize Anomalies")
-        anomaly_hosts_kmeans = df[df['cluster_kmeans'] == 0].index
-        anomaly_hosts_dbscan = df[df['cluster_dbscan'] != 0].index
-        anomaly_hosts_intersection = anomaly_hosts_kmeans.intersection(anomaly_hosts_dbscan)
-
-        plt.figure(figsize=(10, 6))
-        plt.scatter(df['method_post'], df['status_404'], c='blue', label='Normal')
-        plt.scatter(df.loc[anomaly_hosts_intersection, 'method_post'], df.loc[anomaly_hosts_intersection, 'status_404'], c='red', label='Anomalies')
-        plt.xlabel('method_post')
-        plt.ylabel('status_404')
-        plt.legend()
-        plt.title('Anomalies Visualization')
-        st.pyplot()
-
-        # Display anomaly entities
-        st.subheader("Anomaly Entities")
-        anomaly_entities_df = df.loc[anomaly_hosts_intersection, ['method_cnt', 'method_post', 'protocol_1_0', 'status_major', 'status_404', 'status_499', 'status_cnt', 'path_same', 'path_xmlrpc', 'ua_cnt', 'has_payload', 'bytes_avg', 'bytes_std']]
-        st.dataframe(anomaly_entities_df)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
